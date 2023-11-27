e36a1c03015b435dce4649aad6167f2670b797c0
Mostakimw <scientist.expert@gmail.com>
2023-11-27 16:47:58 +0600

    index js route for write routing codes

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,16 @@
 import express, { Application } from 'express';
 const app: Application = express();
 import cors from 'cors';
-import { StudentRoutes } from './app/modules/student/student.route';
-import { UserRoutes } from './app/modules/user/user.route';
 import globalError from './app/middlewares/globalErrorHandler';
 import notFound from './app/middlewares/notFound';
+import router from './app/routes';
 
 // parsers
 app.use(express.json());
 app.use(cors());
 
 //! application routes
-app.use('/api/v1/students', StudentRoutes);
-app.use('/api/v1/users', UserRoutes);
-
+app.use('/api/v1', router);
 //! error handler
 app.use(globalError);
 
