ab9a2cfbb18dd931d8637dede0c9cd7c765cd9e8
Mostakimw <scientist.expert@gmail.com>
2023-11-29 15:08:07 +0600

    services added for semester

--- src/app/modules/academicSemester/academicSemester.controller.ts
+++ src/app/modules/academicSemester/academicSemester.controller.ts
@@ -1,10 +1,12 @@
 import catchAsync from '../../utils/catchAsync';
 import httpStatus from 'http-status';
 import sendResponse from '../../utils/sendResponse';
+import { AcademicSemesterServices } from './academicSemester.services';
 
 const createAcademicSemester = catchAsync(async (req, res) => {
   const semesterData = req.body;
-  const result = await AcademicSemesterServices;
+  const result =
+    await AcademicSemesterServices.createAcademicSemesterIntoDB(semesterData);
 
   sendResponse(res, {
     statusCode: httpStatus.OK,
