9cdeb3ff947d0009c5788eb97f108e7e24997518
Mostakimw <scientist.expert@gmail.com>
2023-11-30 20:40:52 +0600

    academic faculty interface, model, validation created

--- src/app/modules/user/user.utils.ts
+++ src/app/modules/user/user.utils.ts
@@ -15,14 +15,29 @@ const findLastStudentId = async () => {
     .sort({ createdAt: -1 })
     .lean();
 
-  return lastStudent?.id ? lastStudent?.id.substring(6) : undefined;
+  return lastStudent?.id ? lastStudent?.id : undefined;
 };
 
 //generate student id
 // pattern ---> year code 4digit
 export const generatedStudentId = async (payload: TAcademicSemester) => {
   // first id 0000
-  const currentId = (await findLastStudentId()) || (0).toString();
+  let currentId = (0).toString(); // 0000 by default
+
+  // 2030010001
+  const lastStudentId = await findLastStudentId();
+  const lastStudentSemesterCode = lastStudentId?.substring(4, 6); // 01
+  const lastStudentYear = lastStudentId?.substring(0, 4); // 2030
+  const currentSemesterCode = payload.code;
+  const currentYear = payload.year;
+  if (
+    lastStudentId &&
+    lastStudentSemesterCode === currentSemesterCode &&
+    lastStudentYear === currentYear
+  ) {
+    currentId = lastStudentId.substring(6); // 0001
+  }
+
   let incrementId = (Number(currentId) + 1).toString().padStart(4, '0');
 
   incrementId = `${payload.year}${payload.code}${incrementId}`;
