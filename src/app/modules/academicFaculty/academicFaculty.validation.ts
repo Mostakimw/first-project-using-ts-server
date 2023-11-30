098fcca4d6639878dc366e6e58f4362ab6c9c0c6
Mostakimw <scientist.expert@gmail.com>
2023-11-30 21:31:33 +0600

    routes added

--- src/app/modules/academicFaculty/academicFaculty.validation.ts
+++ src/app/modules/academicFaculty/academicFaculty.validation.ts
@@ -5,7 +5,13 @@ const createAcademicFacultyValidationSchema = z.object({
     invalid_type_error: 'Password must be a String',
   }),
 });
+const updateAcademicFacultyValidationSchema = z.object({
+  name: z.string({
+    invalid_type_error: 'Password must be a String',
+  }),
+});
 
 export const AcademicFacultyValidations = {
   createAcademicFacultyValidationSchema,
+  updateAcademicFacultyValidationSchema,
 };
