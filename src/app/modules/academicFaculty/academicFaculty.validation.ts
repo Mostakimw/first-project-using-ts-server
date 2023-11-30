9ad07ea44b704695d15c162e739368c11f6981f7
Mostakimw <scientist.expert@gmail.com>
2023-11-30 21:06:49 +0600

    faculty services added

--- src/app/modules/academicFaculty/academicFaculty.validation.ts
+++ src/app/modules/academicFaculty/academicFaculty.validation.ts
@@ -1,7 +1,9 @@
 import { z } from 'zod';
 
 const createAcademicFacultyValidationSchema = z.object({
-  name: z.string(),
+  name: z.string({
+    invalid_type_error: 'Password must be a String',
+  }),
 });
 
 export const AcademicFacultyValidations = {
