462e17e6bf0cebe2565870cb290f36e7328b4056
Mostakimw <scientist.expert@gmail.com>
2023-12-01 13:24:14 +0600

    app error organized

--- src/app/modules/academicDepartment/academicDepartment.model.ts
+++ src/app/modules/academicDepartment/academicDepartment.model.ts
@@ -1,5 +1,7 @@
 import { Schema, model } from 'mongoose';
 import { TAcademicDepartment } from './academicDepartment.interface';
+import AppError from '../../errors/AppError';
+import httpStatus from 'http-status';
 
 export const academicDepartmentSchema = new Schema<TAcademicDepartment>({
   name: {
@@ -13,26 +15,16 @@ export const academicDepartmentSchema = new Schema<TAcademicDepartment>({
   },
 });
 
-class AppError extends Error {
-  public statusCode: number;
-  constructor(statusCode: number, message: string, stack = '') {
-    super(message);
-    this.statusCode = statusCode;
-    if (stack) {
-      this.stack = stack;
-    } else {
-      Error.captureStackTrace(this, this.constructor);
-    }
-  }
-}
-
 // is department exist pre hook
 academicDepartmentSchema.pre('save', async function (next) {
   const isDepartmentExist = await AcademicDepartment.findOne({
     name: this.name,
   });
   if (isDepartmentExist) {
-    throw new Error('This Department is already exist');
+    throw new AppError(
+      httpStatus.NOT_FOUND,
+      'This Department is already exist',
+    );
   }
   next();
 });
@@ -42,7 +34,7 @@ academicDepartmentSchema.pre('findOneAndUpdate', async function (next) {
   const query = this.getQuery();
   const isDepartmentExist = await AcademicDepartment.findOne(query);
   if (!isDepartmentExist) {
-    throw new AppError(404, 'This Department does not exist');
+    throw new AppError(httpStatus.NOT_FOUND, 'This Department does not exist');
   }
   next();
 });
