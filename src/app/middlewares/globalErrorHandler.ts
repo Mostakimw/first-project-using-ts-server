38ad0987f5c9f10334b441233726b15898ebc81d
Mostakimw <scientist.expert@gmail.com>
2023-12-15 22:17:09 +0600

    zod error handled

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -3,18 +3,41 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
 import { NextFunction, Request, Response } from 'express';
+import { ZodError, ZodIssue } from 'zod';
+import { TErrorSources } from '../interface/error';
+import config from '../config';
+import handleZodError from '../errors/handleZodError';
 const globalError = (
   err: any,
   req: Request,
   res: Response,
   next: NextFunction,
 ) => {
-  const statusCode = err.statusCode || 500;
-  const message = err.message || 'Something went wrong';
+  //default error
+  let statusCode = err.statusCode || 500;
+  let message = err.message || 'Something went wrong!';
+
+  let errorSources: TErrorSources = [
+    {
+      path: '',
+      message: 'Something went wrong!',
+    },
+  ];
+
+  // checking if error from zod
+  if (err instanceof ZodError) {
+    const simplifiedError = handleZodError(err);
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSources = simplifiedError?.errorSources;
+  }
+
+  // ultimate return from here
   return res.status(statusCode).json({
     success: false,
     message,
-    error: err,
+    error: errorSources,
+    stack: config.NODE_ENV === 'development' ? err?.stack : null,
   });
 };
 
